8df7bdd225f03ae099e4c2507cf5451d3a7626a2
TeleKxd <piskulin.i@mail.ru>
2024-11-10 20:25:31 +0300

    Task7

--- Tyuiu.PiskulinIY.Sprint4.Task7.V28.Test/DataServiceTest.cs
+++ Tyuiu.PiskulinIY.Sprint4.Task7.V28.Test/DataServiceTest.cs
@@ -7,6 +7,15 @@ namespace Tyuiu.PiskulinIY.Sprint4.Task7.V28.Test
         [TestMethod]
         public void TestMethod1()
         {
+            DataService ds  = new DataService();
+
+            int rows = 5;
+            int columns = 3;
+            int[,] matrix = new int[rows, columns];
+            string str = "623351179845632";
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 4608;
+            Assert.AreEqual(wait, res);
         }
     }
 }
--- Tyuiu.PiskulinIY.Sprint4.Task7.V28/Program.cs
+++ Tyuiu.PiskulinIY.Sprint4.Task7.V28/Program.cs
@@ -5,7 +5,45 @@ namespace Tyuiu.PiskulinIY.Sprint4.Task7.V28
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода\\вывода в консольных приложениях                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Пискулин Игорь Юрьевич | ИСТНб-24-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат, и печатает его на экране.                          *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int rows = 5;
+            int columns = 3;
+            int[,] matrix = new int[rows, columns];
+            string str = "623351179845632";
+
+            int index = 0;
+            Console.WriteLine("\nМассив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{str[index]} \t");
+                    index++;
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Произведение четных чисел = " + ds.Calculate(rows, columns, str));
+
+            Console.ReadKey();
         }
     }
 }
